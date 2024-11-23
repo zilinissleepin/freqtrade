85ea29c93c31c1d00d959752d3fae5bb31c81842
Matthias <xmatthias@outlook.com>
2024-11-23 14:05:52 +0100

    fix: pydantic - typing_extensions type-dict

--- freqtrade/rpc/api_server/webserver_bgwork.py
+++ freqtrade/rpc/api_server/webserver_bgwork.py
@@ -1,5 +1,7 @@
-from typing import Any, Literal, NotRequired, TypedDict
+from typing import Any, Literal, NotRequired
 from uuid import uuid4
+
+from typing_extensions import TypedDict
 
 from freqtrade.exchange.exchange import Exchange
 
