cc7b1a5642d761b7793713ed911a1de11c57b4fc
Matthias <xmatthias@outlook.com>
2024-12-13 13:30:36 +0100

    chore: clean up install_windows call

--- build_helpers/install_windows.ps1
+++ build_helpers/install_windows.ps1
@@ -1,8 +1,7 @@
 # vendored Wheels compiled via https://github.com/xmatthias/ta-lib-python/tree/ta_bundled_040
 
-python -m pip install --upgrade pip wheel
-
-$pyv = python -c "import sys; print(f'{sys.version_info.major}.{sys.version_info.minor}')"
+python -m pip install --upgrade pip
+python -c "import sys; print(f'{sys.version_info.major}.{sys.version_info.minor}')"
 
 pip install -U wheel "numpy<2"
 pip install --no-build --find-links=build_helpers\ ta-lib
