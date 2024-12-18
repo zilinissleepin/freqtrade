85af1c91a2b3283268eb66aacc9406a63f12d9e1
Matthias <xmatthias@outlook.com>
2024-12-18 06:14:37 +0100

    feat: enable cross futures on binance

--- freqtrade/exchange/binance.py
+++ freqtrade/exchange/binance.py
@@ -57,7 +57,7 @@ class Binance(Exchange):
     _supported_trading_mode_margin_pairs: list[tuple[TradingMode, MarginMode]] = [
         # TradingMode.SPOT always supported and not required in this list
         # (TradingMode.MARGIN, MarginMode.CROSS),
-        # (TradingMode.FUTURES, MarginMode.CROSS),
+        (TradingMode.FUTURES, MarginMode.CROSS),
         (TradingMode.FUTURES, MarginMode.ISOLATED),
     ]
 
