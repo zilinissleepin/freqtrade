9efa48f2a84b4ea9a4aa02853f55bcf5e03b663a
Matthias <xmatthias@outlook.com>
2025-01-22 19:03:33 +0100

    chore: ensure that logged message actually make sense

--- freqtrade/freqai/freqai_interface.py
+++ freqtrade/freqai/freqai_interface.py
@@ -773,7 +773,7 @@ class IFreqaiModel(ABC):
         """
         current_pairlist = self.config.get("exchange", {}).get("pair_whitelist")
         if not self.dd.pair_dict:
-            logger.info("Set fresh train queue from whitelist. Queue: {current_pairlist}")
+            logger.info(f"Set fresh train queue from whitelist. Queue: {current_pairlist}")
             return deque(current_pairlist)
 
         best_queue = deque()
@@ -789,7 +789,7 @@ class IFreqaiModel(ABC):
                 best_queue.appendleft(pair)
 
         logger.info(
-            "Set existing queue from trained timestamps. Best approximation queue: {best_queue}"
+            f"Set existing queue from trained timestamps. Best approximation queue: {best_queue}"
         )
         return best_queue
 
