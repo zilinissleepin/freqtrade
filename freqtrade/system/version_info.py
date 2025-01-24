120194ff1e9195d18b7a3953ffa84fe23a5ad6b1
Matthias <xmatthias@outlook.com>
2025-01-24 19:25:58 +0100

    chore: improved formatting

--- freqtrade/system/version_info.py
+++ freqtrade/system/version_info.py
@@ -8,8 +8,8 @@ def print_version_info():
 
     import ccxt
 
-    print(f"Operating System: \t{platform.platform()}")
-    print(f"Python Version: \tPython {sys.version.split(' ')[0]}")
-    print(f"CCXT Version: \t\t{ccxt.__version__}")
+    print(f"Operating System:\t{platform.platform()}")
+    print(f"Python Version:\t\tPython {sys.version.split(' ')[0]}")
+    print(f"CCXT Version:\t\t{ccxt.__version__}")
     print()
     print(f"Freqtrade Version:\tfreqtrade {__version__}")
