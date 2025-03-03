e055feb1d3d0408e3a964c4f355376f00a6ca19d
Axel-CH <axel.cherubin@gmail.com>
2025-03-03 15:32:41 -0400

    chore: update test_process_trade_creation test

--- tests/freqtradebot/test_freqtradebot.py
+++ tests/freqtradebot/test_freqtradebot.py
@@ -701,11 +701,11 @@ def test_process_trade_creation(
     assert pytest.approx(trade.amount) == 0
     assert pytest.approx(trade.amount_requested) == 60 / ticker_usdt.return_value[ticker_side]
 
-    assert log_has(
+    expected_log = (
         f"{'Short' if is_short else 'Long'} signal found: about create a new trade for ETH/USDT "
-        "with stake_amount: 60.0 ...",
-        caplog,
+        "with stake_amount: 60.0"
     )
+    assert expected_log in caplog.text
     mocker.patch("freqtrade.freqtradebot.FreqtradeBot._check_and_execute_exit")
 
     # Fill trade.
