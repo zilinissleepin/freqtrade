e94ccece93cd71ece3245974192ec58e3a48e76c
mrpabloyeah <mr.pablo.yeah@gmail.com>
2025-03-13 13:51:35 +0100

    Add relevant metrics to backtest breakdowns

--- freqtrade/optimize/optimize_reports/bt_output.py
+++ freqtrade/optimize/optimize_reports/bt_output.py
@@ -132,18 +132,18 @@ def text_table_periodic_breakdown(
     """
     headers = [
         period.capitalize(),
+        "Trades",
         f"Tot Profit {stake_currency}",
-        "Wins",
-        "Draws",
-        "Losses",
+        "Profit Factor",
+        "Win  Draw  Loss  Win%",
     ]
     output = [
         [
             d["date"],
+            d["trades"],
             fmt_coin(d["profit_abs"], stake_currency, False),
-            d["wins"],
-            d["draws"],
-            d["loses"],
+            d["profit_factor"],
+            generate_wins_draws_losses(d["wins"], d["draws"], d["losses"]),
         ]
         for d in days_breakdown_stats
     ]
