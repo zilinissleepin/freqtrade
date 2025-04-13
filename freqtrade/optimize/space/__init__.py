30ead79e112e5c33afa873ad332b2a033c74efe4
viotemp1 <mr_vii@icloud.com>
2025-04-13 11:22:07 +0300

    fixed freqtrade/optimize/space/__init__.py:1:66: RUF100 [*] Unused `noqa` directive (unused: `F401`)

--- freqtrade/optimize/space/__init__.py
+++ freqtrade/optimize/space/__init__.py
@@ -1,4 +1,3 @@
-# from skopt.space import Categorical, Dimension, Integer, Real  # noqa: F401
 from .decimalspace import SKDecimal  # noqa: F401
 from .optunaspaces import DimensionProtocol as Dimension  # noqa: F401
 from .optunaspaces import ft_CategoricalDistribution as Categorical  # noqa: F401
