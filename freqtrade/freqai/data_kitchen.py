12728c0fccabcf2a44effc44a9cd76ff07889218
James Lin <jameslin1998@gmail.com>
2025-04-26 10:18:39 -0400

    fix

--- freqtrade/freqai/data_kitchen.py
+++ freqtrade/freqai/data_kitchen.py
@@ -712,7 +712,7 @@ class FreqaiDataKitchen:
         for s in ['trades', 'orderflow', 'imbalances', 'stacked_imbalances_bid',
                     'stacked_imbalances_ask', 'max_delta', 'min_delta', 'bid', 'ask',
                     'delta', 'total_trades']:
-            if s in dataframe.columns and f"{s}_{suffix}" in df_to_merge.columns:
+            if s in dataframe.columns and f"{s}_{suffix}" in dataframe.columns:
                 skip_columns.append(f"{s}_{suffix}")
         dataframe = dataframe.drop(columns=skip_columns)
         return dataframe
