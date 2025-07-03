1e24653e798e7c9064f74e6efc71967807282899
Matthias <xmatthias@outlook.com>
2025-07-03 08:34:38 +0200

    feat: add luno basic exchange configuration
    closes #11928

--- freqtrade/exchange/__init__.py
+++ freqtrade/exchange/__init__.py
@@ -43,4 +43,5 @@ from freqtrade.exchange.idex import Idex
 from freqtrade.exchange.kraken import Kraken
 from freqtrade.exchange.kucoin import Kucoin
 from freqtrade.exchange.lbank import Lbank
+from freqtrade.exchange.luno import Luno
 from freqtrade.exchange.okx import Okx
