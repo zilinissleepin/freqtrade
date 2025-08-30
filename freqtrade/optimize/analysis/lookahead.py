539937c0eb58948897f936335754c2e995abb3cd
Matthias <xmatthias@outlook.com>
2025-08-30 11:47:38 +0200

    refactor: move base_analysis to where it belongs

--- freqtrade/optimize/analysis/lookahead.py
+++ freqtrade/optimize/analysis/lookahead.py
@@ -13,8 +13,8 @@ from freqtrade.loggers.set_log_levels import (
     reduce_verbosity_for_bias_tester,
     restore_verbosity_for_bias_tester,
 )
+from freqtrade.optimize.analysis.base_analysis import BaseAnalysis, VarHolder
 from freqtrade.optimize.backtesting import Backtesting
-from freqtrade.optimize.base_analysis import BaseAnalysis, VarHolder
 
 
 logger = logging.getLogger(__name__)
