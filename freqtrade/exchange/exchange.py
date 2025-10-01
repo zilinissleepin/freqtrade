70fa12f1b2b722cd79c9b65d513f08de2661e8a0
Robert Caulk <rob.caulk@gmail.com>
2025-10-01 16:36:55 +0200

    chore: log a warning that the user is in territory that might not work.

--- freqtrade/exchange/exchange.py
+++ freqtrade/exchange/exchange.py
@@ -837,6 +837,11 @@ class Exchange:
             raise ConfigurationError(
                 f"Historic OHLCV data not available for {self.name}. Can't use freqAI."
             )
+        elif override and freqai_enabled and not self._ft_has["ohlcv_has_history"]:
+            logger.warning(
+                "Overriding exchange checks for freqAI. Make sure that your exchange supports "
+                "fetching historic OHLCV data, otherwise freqAI will not work."
+            )
 
     def validate_required_startup_candles(self, startup_candles: int, timeframe: str) -> int:
         """
